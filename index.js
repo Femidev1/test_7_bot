3bd3362d70a5236a18f5b32f1e14a116b65787fd
Femidev1 <128675924+Femidev1@users.noreply.github.com>
2025-01-07 23:47:53 -0500

    debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // Bot Config
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-const frontendUrl = "https://test-7-front.vercel.app";
+const frontendUrl = "https://test-7-frontdev.vercel.app";
 
 // Start Command
 bot.onText(/\/start/, async (msg) => {
@@ -16,9 +16,11 @@ bot.onText(/\/start/, async (msg) => {
     How quickly can you conquer the galaxy?
 
     Tap to fuel your ship, mine tokens, and upgrade your engines.
+
     Boost your passive income, strategize your growth, and rule the multiverse!
 
     Your efforts won’t go unnoticed—big things are coming! 🚀
+
     Don’t forget your crew—invite friends and earn even more together!
 
     Let the cosmic mining begin! 🌠💎`,
